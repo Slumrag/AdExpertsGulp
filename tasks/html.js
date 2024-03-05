6633d9a5de9d840871e03b77954965b252d3dcce
Slumrag <mrgreenthecristal@yandex.ru>
2024-03-05 19:35:11 +0300

    refactor: create reusable bem blocks

--- tasks/html.js
+++ tasks/html.js
@@ -1,13 +1,15 @@
-const {
-	src,
-	dest
-} = require('gulp');
-const include = require('gulp-file-include');
+const { src, dest } = require('gulp');
+const fileinclude = require('gulp-file-include');
 const bs = require('browser-sync');
 
 module.exports = function html() {
-	return src(['src/**/*.html', '!src/components/**/*.html'])
-		.pipe(include())
-		.pipe(dest('build'))
-		.pipe(bs.stream())
-}
+  return src(['src/**/*.html', '!src/components/**/*.html'])
+    .pipe(
+      fileinclude({
+        prefix: '@@',
+        basepath: '@file',
+      })
+    )
+    .pipe(dest('build'))
+    .pipe(bs.stream());
+};
